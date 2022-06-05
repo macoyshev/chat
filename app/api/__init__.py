49aa402524edd70fc302d5b45636285ee24a59ab
macoyshev <make.oynoshev@gmail.com>
2022-06-05 19:31:30 +0300

    cors

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,9 +3,14 @@ from fastapi import FastAPI
 from .routers import chat
 
 
+class CORSMiddleware:
+    pass
+
+
 def create_api() -> FastAPI:
     app = FastAPI()
 
+    app.add_middleware(CORSMiddleware, allow_origin_regex='*')
     app.include_router(chat.router)
 
     return app
