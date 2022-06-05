d508b830348252655c3a026704f5d1913fcea4ab
macoyshev <make.oynoshev@gmail.com>
2022-06-05 14:06:56 +0300

    broad removed

--- app/api/routers/chat.py
+++ app/api/routers/chat.py
@@ -31,7 +31,7 @@ async def websocket_endpoint(
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(data)
+            await websocket.send_text(data)
             await redis.publish('chat', f'#{username}: {data}')
             await redis.rpush('chat', f'#{username}: {data}')
             await redis.ltrim('chat', 0, 49)
